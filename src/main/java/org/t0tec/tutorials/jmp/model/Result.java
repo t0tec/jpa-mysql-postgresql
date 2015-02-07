9f84492d7911689691f87b3dc112e077a316c672
t0tec <t0tec.olmec@gmail.com>
2015-02-07 11:51:18 +0100

    Forgot Serializable

--- src/main/java/org/t0tec/tutorials/jmp/model/Result.java
+++ src/main/java/org/t0tec/tutorials/jmp/model/Result.java
@@ -1,5 +1,7 @@
 package org.t0tec.tutorials.jmp.model;
 
+import java.io.Serializable;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -16,7 +18,7 @@ import javax.persistence.Table;
  */
 @Entity
 @Table(name = "result")
-public class Result {
+public class Result implements Serializable {
 
   @Id
   @GeneratedValue
